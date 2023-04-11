3a64d9d65c406f73f72a3178e5b1e09423201800
gafarovbr <bulat.gafarov95@mail.ru>
2023-04-11 12:31:46 +0300

    Добавил подключение датчика CO2 по ШИМ

--- ППО/temperature_osv_serial/temperature_osv_serial.ino
+++ ППО/temperature_osv_serial/temperature_osv_serial.ino
@@ -4,23 +4,47 @@ iarduino_I2C_SHT sht;                             //   Объявляем объ
 
 
 
-const int pinD27 = 27;
+const int pinD27 = 27;                            // Пин подключения аналогового датчика освещенности
+const int pinD5 = 5;                              // Пин подключение ШИМ для CO2
 
+int prevVal = LOW;
+long th, tl, h, l, ppm;
 
 void setup(){                                     //
     delay(500);                                   // * Ждём завершение переходных процессов связанных с подачей питания.
     Serial.begin(9600);                           //
     while(!Serial){;}                             // * Ждём завершения инициализации шины UART.
     sht.begin();                                  //   Инициируем работу с датчиком температуры и влажности.
-}                                                 //
-                                                  //
-void loop(){   
+    pinMode(pinD5, INPUT);
+
+}                                                 
+                                                  
+void loop(){ 
+    long tt = millis();  
+    int myVal = digitalRead(pinD5);
+    if (myVal == HIGH) {
+      if (myVal != prevVal) {
+        h = tt;
+        tl = h - l;
+        prevVal = myVal;
+      }
+  }  else {
+    if (myVal != prevVal) {
+      l = tt;
+      th = l - h;
+      prevVal = myVal;
+      ppm = 5000 * (th - 2) / (th + tl - 4);
+      //Serial.println("PPM = " + String(ppm));
+    }
+  }
+    Serial.print("PPM= ") ;
+    Serial.print(String(ppm));    
     Serial.print("Освещенность = ");
     Serial.print(analogRead(pinD27));                     
-    Serial.print("Температура = ");               //
+    Serial.print("Температура = ");               
     Serial.print( sht.getTem() );                 //   Выводим текущую температуру, от -40.0 до +125 °C.
-    Serial.print(" °C, влажность = ");            //
+    Serial.print(" °C, влажность = ");            
     Serial.print( sht.getHum() );                 //   Выводим текущую влажность воздуха, от 0 до 100%.
-    Serial.print(" %.\r\n");                      //
+    Serial.print(" %.\r\n");                      
     delay(500);                                   // * Задержка позволяет медленнее заполнять монитор последовательного порта.
 } 
